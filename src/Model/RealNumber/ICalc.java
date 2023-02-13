5aded23a4a91ceffd6d2c09f7e41bc6c281fe76d
Kirill <cirill@mail.ru>
2023-02-13 21:27:41 +0300

    hw_5

---
+++ src/Model/RealNumber/ICalc.java
@@ -0,0 +1,12 @@
+
+package Model.RealNumber;
+
+/**
+ * Интерфейс математической операции
+ */
+public interface ICalc {
+    @SafeVarargs
+    public static <T> double result(T... args) {
+        return 0;
+    }
+}
